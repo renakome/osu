b7ff3d143484d1f23051b38b45b1f04b5263ba0c
Renako <131531729+renakome@users.noreply.github.com>
2025-12-27 14:08:25 -0300

    Update OsuModTouchDevice.cs

--- osu.Game.Rulesets.Osu/Mods/OsuModTouchDevice.cs
+++ osu.Game.Rulesets.Osu/Mods/OsuModTouchDevice.cs
@@ -3,6 +3,8 @@
 
 using System;
 using System.Linq;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
 using osu.Game.Rulesets.Mods;
 using osu.Game.Rulesets.UI;
 using osu.Game.Rulesets.Osu.UI;
